cabd087ee55694f2ff4b122f145db3122072b3ee
Flamur Deliu <f.deliu@sitel.com.al>
2020-06-12 17:57:50 +0200

    scaffolding the app

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span>Projects </span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+         <v-icon >exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+  </nav>
+</template>
+
+<script>
+export default{
+    
+}
+</script>
+
+<style scoped>
+
+</style>
